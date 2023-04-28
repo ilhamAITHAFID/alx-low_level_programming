d78ee1ce8a563d841c4f623f24ef88fb2dca1f6c
ilhamAITHAFID <i.ait_hafid@mundiapolis.ma>
2023-04-28 09:27:19 -0700

    task 1 update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,6 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*while (i < 10)*/
@@ -14,6 +13,5 @@ int main(void)
 	/*putchar(i);*/
 	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
-}	
+}
